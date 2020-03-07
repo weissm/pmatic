c8309392692b038d22f39bbce29c9946b315aca3
Matthias Weiss <matthias.h.weiss@arcor.de>
2020-03-07 13:39:03 +0100

    added email handling for notification

--- pmatic/notify.py
+++ pmatic/notify.py
@@ -41,6 +41,46 @@ import json
 import pmatic.utils as utils
 from pmatic.exceptions import PMException, PMUserError
 
+# mailer class from https://gist.github.com/dtanham/11326557
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+class Mailer:
+    def __init__(self, **kwargs):
+        mandatory_args = ["username","password","server","port"]
+        for x in mandatory_args:
+            if kwargs.get(x, False) == False:
+                raise ValueError("%s must be provided" % (x))
+            self.__dict__[x] = kwargs[x]
+
+    def send(self, **kwargs):
+        mandatory_args = ["subject","to","content","content_type"]
+        for x in mandatory_args:
+            if not kwargs.get(x, False):
+                raise ValueError("%s is mandatory" % (x))
+
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = kwargs['subject']
+        msg['From'] = self.username
+        msg['To'] = kwargs['to']
+
+        content = MIMEText(kwargs['content'], kwargs['content_type'])
+        msg.attach(content)
+        s = smtplib.SMTP(self.server, self.port)
+        s.starttls()
+        s.login(self.username, self.password)
+        s.sendmail(msg['From'], msg['To'], msg.as_string())
+        s.quit()
+
+    def sendHTML(self, **kwargs):
+        kwargs['content_type'] = "html"
+        return self.send(**kwargs)
+
+    def sendText(self, **kwargs):
+        kwargs['content_type'] = "plain"
+        return self.send(**kwargs)
 
 
 class Pushover(utils.LogMixin):
