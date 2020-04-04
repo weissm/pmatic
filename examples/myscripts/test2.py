6030e93de798cee8dc3af6ea48a30eb2e12d7709
weissm <matthias.h.weiss@arcor.de>
2020-04-04 13:45:28 +0200

    Delete test2.py

--- examples/myscripts/test2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import pmatic
-ccu = pmatic.CCU(address="http://ccu3-webui", credentials=("weissm", "micro23"))
-
-print("Low battery: ")
-print(len(ccu.CCUDevices))
-for device in ccu.CCUDevices:
-    print("  %s" % device.name)
